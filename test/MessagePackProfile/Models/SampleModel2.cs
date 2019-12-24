53c313162ca78f2a14b68caaaaf2b4e93e3b22f8
BigOyayubi <55793780+BigOyayubi@users.noreply.github.com>
2019-12-24 17:41:13 +0900

    add profile patterns

---
+++ test/MessagePackProfile/Models/SampleModel2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessagePackProfile.Models
+{
+    [MessagePack.MessagePackObject]
+    public class SampleModel2
+    {
+        public SampleModel2 Set(int i)
+        {
+            Key = i.ToString();
+            IntValue = i;
+            FloatValue = (float)i;
+            DoubleValue = (double)i;
+            return this;
+        }
+
+        [MessagePack.Key(0)]
+        public string Key { get; set; }
+        [MessagePack.Key(1)]
+        public int IntValue { get; set; }
+        [MessagePack.Key(2)]
+        public float FloatValue { get; set; }
+        [MessagePack.Key(3)]
+        public double DoubleValue { get; set; }
+    }
+}
--- test/MessagePackProfile/SerializerBenchmark.cs
+++ test/MessagePackProfile/SerializerBenchmark.cs
@@ -5,34 +5,112 @@ using System.Collections.Generic;
 using BenchmarkDotNet;
 using BenchmarkDotNet.Attributes;
 using MiniMessagePack;
+using FlatBuffers;
 
 namespace MessagePackProfile
 {
     [Config(typeof(BenchmarkConfig))]
     public class SerializerBenchmark
     {
-        private byte[] ArrayModelSerialized;
-        private string ArrayModelJsonString;
+        private byte[] ArrayModelSerialized;    //for MsgPackCli, MiniMessagePack, MessagePack-CSharp
+        private byte[] ArrayModelSerializedIdx; //for MessagePack-CSharp KeyAttribute
+        private byte[] ArrayModelFbsSerialized; //for flatbuffers
+        private string ArrayModelJsonString;    //for MiniJson, Utf8Json
+        private const int ArraySize = 1000;
 
         [GlobalSetup]
         public void Setup()
         {
+            SetupCommon();
+            SetupMessagePackCSharpKeyAttr();
+            SetupFbs();
+        }
+
+        private void SetupCommon()
+        {
+#if false
             var resolver = MessagePack.Resolvers.CompositeResolver.Create(
                 MessagePack.Resolvers.ContractlessStandardResolver.Instance,
                 MessagePack.Resolvers.StandardResolver.Instance
                 );
             var options = MessagePack.MessagePackSerializerOptions.Standard.WithResolver(resolver);
             MessagePack.MessagePackSerializer.DefaultOptions = options;
-
+#endif
             IList<Models.SampleModel1> arrayObjects = Enumerable.Range(0, 1000).Select(_ => new Models.SampleModel1().Set(_)).ToList();
             this.ArrayModelSerialized = MessagePack.MessagePackSerializer.Serialize(arrayObjects);
+
             this.ArrayModelJsonString = MessagePack.MessagePackSerializer.SerializeToJson(arrayObjects);
+
+        }
+
+        private void SetupMessagePackCSharpKeyAttr()
+        {
+            IList<Models.SampleModel2> arrayObjects = Enumerable.Range(0, 1000).Select(_ => new Models.SampleModel2().Set(_)).ToList();
+            this.ArrayModelSerializedIdx = MessagePack.MessagePackSerializer.Serialize(arrayObjects);
+        }
+
+        private void SetupFbs()
+        {
+            var builder = new FlatBuffers.FlatBufferBuilder(1024);
+            var elementOffsets = new Offset<flatbuffers.SampleModel1Element>[1000];
+            foreach (var i in Enumerable.Range(0, 1000))
+            {
+                elementOffsets[i] = flatbuffers.SampleModel1Element.CreateSampleModel1Element(
+                    builder,
+                    builder.CreateString(i.ToString()),
+                    i,
+                    (float)i,
+                    (double)i
+                );
+            }
+
+            var elements = flatbuffers.SampleModel1Array.CreateElementsVector(builder, elementOffsets);
+
+            flatbuffers.SampleModel1Array.StartSampleModel1Array(builder);
+            {
+                flatbuffers.SampleModel1Array.AddElements(
+                    builder,
+                    elements
+                );
+            }
+            var orc = flatbuffers.SampleModel1Array.EndSampleModel1Array(builder);
+            builder.Finish(orc.Value);
+
+            this.ArrayModelFbsSerialized = builder.SizedByteArray();
+        }
+
+        [Benchmark]
+        public void Array_Flatbuffers()
+        {
+            var deserialize = flatbuffers.SampleModel1Array.GetRootAsSampleModel1Array(new ByteBuffer(this.ArrayModelFbsSerialized));
+            var length = deserialize.ElementsLength;
+            for(int i = 0; i< length; i++)
+            {
+                var value = deserialize.Elements(i);
+                var kv = value.Value.Key;
+                var iv = value.Value.IntValue;
+                var fv = value.Value.FloatValue;
+            }
         }
 
         [Benchmark]
         public void Array_MessagePackCSharp()
         {
             var deserialize = MessagePack.MessagePackSerializer.Deserialize<IList<Models.SampleModel1>>(this.ArrayModelSerialized);
+            var length = deserialize.Count;
+            for (int i = 0; i < length; i++)
+            {
+                var value = deserialize[i];
+                var kv = value.Key;
+                var iv = value.IntValue;
+                var fv = value.FloatValue;
+            }
+        }
+
+        [Benchmark]
+        public void Array_MessagePackCSharpKeyAttr()
+        {
+            var deserialize = MessagePack.MessagePackSerializer.Deserialize<IList<Models.SampleModel2>>(this.ArrayModelSerializedIdx);
             var length = deserialize.Count;
             for (int i = 0; i < length; i++)
             {
